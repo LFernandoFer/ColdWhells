8c7fce57bf36370b7bc9f49318ae873a5f1f76d7
LFernandoFer <luis.fernando.ferreira.junior@gmail.com>
2025-06-08 17:01:35 -0300

    Add mudança de icones

--- ColdWheels/Views/pgListaCarro.xaml.cs
+++ ColdWheels/Views/pgListaCarro.xaml.cs
@@ -27,12 +27,21 @@ public partial class pgListaCarro : ContentPage
 
     private void tapFiltrarObtido_Tapped(object sender, TappedEventArgs e)
     {
-
+        if (!ReturnObtido())
+        {
+            imgObtido.Source = "car_check.png";
+        }
+        else
+            imgObtido.Source = "car.png";
     }
-
     private void tapFiltrarDesejado_Tapped(object sender, TappedEventArgs e)
     {
-
+        if(!ReturnDesejado())
+        {
+            imgDesejado.Source = "heart_check.png";
+        }
+        else
+            imgDesejado.Source = "heart.png";
     }
     private void tapDeletar_Tapped(object sender, TappedEventArgs e)
     {
@@ -42,6 +51,32 @@ public partial class pgListaCarro : ContentPage
     private void btnFiltrar_Clicked(object sender, EventArgs e)
     {
         string nome = txtFiltroNome.Text;
+        var stsObtido = imgObtido.Source as FileImageSource;
+        var stsDesejado = imgDesejado.Source as FileImageSource;
 
+        //controller.Filtrar(nome, );
+
+    }
+
+    private bool ReturnDesejado()
+    {
+        var img = imgDesejado.Source as FileImageSource;
+
+        if (img.File != null && img.File == "heart.png")
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private bool ReturnObtido()
+    {
+        var img = imgObtido.Source as FileImageSource;
+
+        if (img.File != null && img.File == "car.png")
+        {
+            return false;
+        }
+        return true;
     }
 }
